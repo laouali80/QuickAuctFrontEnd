c49004ad9b4fd47bfa79087fe5171f639dc78b60
Laouali <laoualibachir2000@gmail.com>
2025-03-17 22:21:54 +0100

    style the forms layout

--- auction-components/SignInForm.js
+++ auction-components/SignInForm.js
@@ -48,10 +48,10 @@ const SignInForm = () => {
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const handleChange = (name, value) => {
-    setFormData({ ...formData, [name]: value });
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = () => {
+  const validateForm = () => {
     let newErrors = {};
 
     if (!formData.firstName) newErrors.firstName = "First Name is required.";
@@ -67,172 +67,373 @@ const SignInForm = () => {
       newErrors.termsAccepted = "You must accept the terms.";
 
     setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
-    if (Object.keys(newErrors).length === 0) {
+  const handleSubmit = () => {
+    if (validateForm()) {
       console.log("Form submitted:", formData);
     }
   };
 
   return (
-    <ScrollView>
-      <VStack className="w-full p-4">
-        <FormControl isInvalid={!!errors.firstName}>
-          <VStack space="xs">
-            <Text className="text-typography-500">First Name</Text>
+    <VStack className="px-8 py-4">
+      <FormControl size="md" className="gap-y-4">
+        {[
+          { label: "First Name", name: "firstName" },
+          { label: "Last Name", name: "lastName" },
+          { label: "Username", name: "username" },
+          {
+            label: "Email",
+            name: "email",
+            props: { keyboardType: "email-address" },
+          },
+          {
+            label: "Phone Number",
+            name: "phoneNumber",
+            props: { keyboardType: "phone-pad" },
+          },
+        ].map(({ label, name, props }) => (
+          <VStack space="xs" key={name}>
+            <Text className="text-typography-500">{label}</Text>
             <Input className="min-w-[250px]">
               <InputField
                 type="text"
-                value={formData.firstName}
-                onChangeText={(text) => handleChange("firstName", text)}
+                value={formData[name]}
+                onChangeText={(text) => handleChange(name, text)}
+                {...props}
               />
             </Input>
-            {errors.firstName && (
-              <FormControlErrorText>{errors.firstName}</FormControlErrorText>
+            {errors[name] && (
+              <FormControlErrorText>{errors[name]}</FormControlErrorText>
             )}
           </VStack>
-        </FormControl>
+        ))}
 
-        <FormControl isInvalid={!!errors.lastName}>
-          <VStack space="xs">
-            <Text className="text-typography-500">Last Name</Text>
-            <Input className="min-w-[250px]">
-              <InputField
-                type="text"
-                value={formData.lastName}
-                onChangeText={(text) => handleChange("lastName", text)}
-              />
-            </Input>
-            {errors.lastName && (
-              <FormControlErrorText>{errors.lastName}</FormControlErrorText>
-            )}
-          </VStack>
-        </FormControl>
-
-        <FormControl isInvalid={!!errors.username}>
-          <VStack space="xs">
-            <Text className="text-typography-500">Username</Text>
-            <Input className="min-w-[250px]">
-              <InputField
-                type="text"
-                value={formData.username}
-                onChangeText={(text) => handleChange("username", text)}
-              />
-            </Input>
-            {errors.username && (
-              <FormControlErrorText>{errors.username}</FormControlErrorText>
-            )}
-          </VStack>
-        </FormControl>
-
-        <FormControl isInvalid={!!errors.email}>
-          <VStack space="xs">
-            <Text className="text-typography-500">Email</Text>
-            <Input className="min-w-[250px]">
-              <InputField
-                type="text"
-                value={formData.email}
-                onChangeText={(text) => handleChange("email", text)}
-              />
-            </Input>
-            {errors.email && (
-              <FormControlErrorText>{errors.email}</FormControlErrorText>
-            )}
-          </VStack>
-        </FormControl>
-
-        <FormControl isInvalid={!!errors.password}>
-          <VStack space="xs">
+        {/* Password Fields */}
+        {[
+          { label: "Password", name: "password", state: showPassword },
+          {
+            label: "Confirm Password",
+            name: "confirmPassword",
+            state: showConfirmPassword,
+          },
+        ].map(({ label, name, state }) => (
+          <VStack space="xs" key={name}>
             <FormControlLabel>
-              <FormControlLabelText>Password</FormControlLabelText>
+              <FormControlLabelText>{label}</FormControlLabelText>
             </FormControlLabel>
             <Input className="text-center">
               <InputField
-                type={showPassword ? "text" : "password"}
-                value={formData.password}
-                onChangeText={(text) => handleChange("password", text)}
+                type={state ? "text" : "password"}
+                value={formData[name]}
+                onChangeText={(text) => handleChange(name, text)}
               />
               <InputSlot
                 className="pr-3"
-                onPress={() => setShowPassword(!showPassword)}
+                onPress={() =>
+                  name === "password"
+                    ? setShowPassword(!showPassword)
+                    : setShowConfirmPassword(!showConfirmPassword)
+                }
               >
-                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+                <InputIcon as={state ? EyeIcon : EyeOffIcon} />
               </InputSlot>
             </Input>
-            {errors.password && (
-              <FormControlErrorText>{errors.password}</FormControlErrorText>
+            {errors[name] && (
+              <FormControlErrorText>{errors[name]}</FormControlErrorText>
             )}
           </VStack>
-        </FormControl>
+        ))}
 
-        <FormControl isInvalid={!!errors.confirmPassword}>
-          <VStack space="xs">
-            <FormControlLabel>
-              <FormControlLabelText>Confirm Password</FormControlLabelText>
-            </FormControlLabel>
-            <Input className="text-center">
-              <InputField
-                type={showConfirmPassword ? "text" : "password"}
-                value={formData.confirmPassword}
-                onChangeText={(text) => handleChange("confirmPassword", text)}
-              />
-              <InputSlot
-                className="pr-3"
-                onPress={() => setShowConfirmPassword(!showConfirmPassword)}
-              >
-                <InputIcon as={showConfirmPassword ? EyeIcon : EyeOffIcon} />
-              </InputSlot>
-            </Input>
-            {errors.confirmPassword && (
-              <FormControlErrorText>
-                {errors.confirmPassword}
-              </FormControlErrorText>
-            )}
-          </VStack>
-        </FormControl>
-
-        <Checkbox
-          size="md"
-          isChecked={formData.termsAccepted}
-          onChange={() =>
-            handleChange("termsAccepted", !formData.termsAccepted)
-          }
-        >
-          <CheckboxIndicator>
-            <CheckboxIcon as={CheckIcon} />
-          </CheckboxIndicator>
-          <CheckboxLabel>I agree to terms and conditions</CheckboxLabel>
-        </Checkbox>
-        {errors.termsAccepted && (
-          <FormControlErrorText>{errors.termsAccepted}</FormControlErrorText>
-        )}
-
-        <Button
-          className="w-fit self-center mt-4"
-          size="sm"
-          onPress={handleSubmit}
-        >
-          <ButtonText>Sign Up</ButtonText>
-        </Button>
-
-        <HStack className="items-center">
-          <Divider className="w-full" />
-          <Text className="font-semibold mx-2">Or</Text>
-          <Divider className="w-full" />
+        {/* Terms Checkbox */}
+        <HStack space="xs">
+          <Checkbox
+            size="md"
+            isChecked={formData.termsAccepted}
+            onChange={() =>
+              handleChange("termsAccepted", !formData.termsAccepted)
+            }
+          >
+            <CheckboxIndicator>
+              <CheckboxIcon as={CheckIcon} />
+            </CheckboxIndicator>
+          </Checkbox>
+          <Pressable>
+            <Text>
+              I agree to{" "}
+              <Text className="text-blue-600">terms and conditions</Text>
+            </Text>
+          </Pressable>
+          {errors.termsAccepted && (
+            <FormControlErrorText>{errors.termsAccepted}</FormControlErrorText>
+          )}
         </HStack>
+      </FormControl>
 
-        <VStack space="sm" className="mt-4">
-          <Pressable className="p-4 border border-outline-200 rounded-full flex-row items-center justify-center">
-            <AntDesign name="google" size={24} color="black" />
-            <Text className="ml-2">Continue with Google</Text>
+      {/* Submit Button */}
+      <Button
+        className="w-full self-center mt-4 bg-[#259e47] rounded-lg"
+        size="sm"
+        onPress={handleSubmit}
+        isDisabled={
+          !formData.firstName ||
+          !formData.lastName ||
+          !formData.username ||
+          !formData.email ||
+          !formData.phoneNumber ||
+          !formData.password ||
+          !formData.confirmPassword ||
+          !formData.termsAccepted
+        }
+      >
+        <ButtonText>Sign Up</ButtonText>
+      </Button>
+
+      {/* OR Divider */}
+      <HStack className="flex items-center my-4">
+        <Divider className="flex-1" />
+        <Text className="font-semibold mx-2">Or</Text>
+        <Divider className="flex-1" />
+      </HStack>
+
+      {/* Social Login Buttons */}
+      <VStack space="sm" className="mt-4">
+        {[
+          { name: "google", label: "Continue with Google" },
+          { name: "apple1", label: "Continue with Apple" },
+        ].map(({ name, label }) => (
+          <Pressable
+            key={name}
+            className="p-4 border border-outline-200 rounded-full flex-row items-center justify-center"
+          >
+            <AntDesign name={name} size={24} color="black" />
+            <Text className="ml-2">{label}</Text>
           </Pressable>
-          <Pressable className="p-4 border border-outline-200 rounded-full flex-row items-center justify-center">
-            <AntDesign name="apple1" size={24} color="black" />
-            <Text className="ml-2">Continue with Apple</Text>
-          </Pressable>
-        </VStack>
+        ))}
       </VStack>
-    </ScrollView>
+    </VStack>
   );
 };
 
 export default SignInForm;
+
+// const SignInForm = () => {
+//   const [formData, setFormData] = useState({
+//     firstName: "",
+//     lastName: "",
+//     username: "",
+//     email: "",
+//     phoneNumber: "",
+//     password: "",
+//     confirmPassword: "",
+//     termsAccepted: false,
+//   });
+
+//   const [isInvalid, setIsInvalid] = useState(false);
+//   const [errors, setErrors] = useState({});
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+//   const handleChange = (name, value) => {
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSubmit = () => {
+//     let newErrors = {};
+
+//     if (!formData.firstName) newErrors.firstName = "First Name is required.";
+//     if (!formData.lastName) newErrors.lastName = "Last Name is required.";
+//     if (!formData.username) newErrors.username = "Username is required.";
+//     if (!formData.email.includes("@"))
+//       newErrors.email = "Invalid email address.";
+//     if (formData.password.length < 6)
+//       newErrors.password = "Password must be at least 6 characters.";
+//     if (formData.confirmPassword !== formData.password)
+//       newErrors.confirmPassword = "Passwords do not match.";
+//     if (!formData.termsAccepted)
+//       newErrors.termsAccepted = "You must accept the terms.";
+
+//     setErrors(newErrors);
+
+//     if (Object.keys(newErrors).length === 0) {
+//       console.log("Form submitted:", formData);
+//     }
+//   };
+
+//   return (
+//     <VStack className="px-8 py-4">
+//       <FormControl isInvalid={isInvalid} size="md" className="gap-y-4">
+//         <VStack space="xs">
+//           <Text className="text-typography-500">First Name</Text>
+//           <Input className="min-w-[250px]">
+//             <InputField
+//               type="text"
+//               value={formData.firstName}
+//               onChangeText={(text) => handleChange("firstName", text)}
+//             />
+//           </Input>
+//           {errors.firstName && (
+//             <FormControlErrorText>{errors.firstName}</FormControlErrorText>
+//           )}
+//         </VStack>
+
+//         <VStack space="xs">
+//           <Text className="text-typography-500">Last Name</Text>
+//           <Input className="min-w-[250px]">
+//             <InputField
+//               type="text"
+//               value={formData.lastName}
+//               onChangeText={(text) => handleChange("lastName", text)}
+//             />
+//           </Input>
+//           {errors.lastName && (
+//             <FormControlErrorText>{errors.lastName}</FormControlErrorText>
+//           )}
+//         </VStack>
+
+//         <VStack space="xs">
+//           <Text className="text-typography-500">Username</Text>
+//           <Input className="min-w-[250px]">
+//             <InputField
+//               type="text"
+//               value={formData.username}
+//               onChangeText={(text) => handleChange("username", text)}
+//             />
+//           </Input>
+//           {errors.username && (
+//             <FormControlErrorText>{errors.username}</FormControlErrorText>
+//           )}
+//         </VStack>
+
+//         <VStack space="xs">
+//           <Text className="text-typography-500">Email</Text>
+//           <Input className="min-w-[250px]">
+//             <InputField
+//               type="text"
+//               value={formData.email}
+//               onChangeText={(text) => handleChange("email", text)}
+//             />
+//           </Input>
+//           {errors.email && (
+//             <FormControlErrorText>{errors.email}</FormControlErrorText>
+//           )}
+//         </VStack>
+
+//         <VStack space="xs">
+//           <Text className="text-typography-500">Phone Number</Text>
+//           <Input className="min-w-[250px]">
+//             <InputField
+//               type="text"
+//               value={formData.phoneNumber}
+//               onChangeText={(text) => handleChange("phoneNumber", text)}
+//             />
+//           </Input>
+//           {errors.phoneNumber && (
+//             <FormControlErrorText>{errors.phoneNumber}</FormControlErrorText>
+//           )}
+//         </VStack>
+
+//         <VStack space="xs">
+//           <FormControlLabel>
+//             <FormControlLabelText>Password</FormControlLabelText>
+//           </FormControlLabel>
+//           <Input className="text-center">
+//             <InputField
+//               type={showPassword ? "text" : "password"}
+//               value={formData.password}
+//               onChangeText={(text) => handleChange("password", text)}
+//             />
+//             <InputSlot
+//               className="pr-3"
+//               onPress={() => setShowPassword(!showPassword)}
+//             >
+//               <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} />
+//             </InputSlot>
+//           </Input>
+//           {errors.password && (
+//             <FormControlErrorText>{errors.password}</FormControlErrorText>
+//           )}
+//         </VStack>
+
+//         <VStack space="xs">
+//           <FormControlLabel>
+//             <FormControlLabelText>Confirm Password</FormControlLabelText>
+//           </FormControlLabel>
+//           <Input className="text-center">
+//             <InputField
+//               type={showConfirmPassword ? "text" : "password"}
+//               value={formData.confirmPassword}
+//               onChangeText={(text) => handleChange("confirmPassword", text)}
+//             />
+//             <InputSlot
+//               className="pr-3"
+//               onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+//             >
+//               <InputIcon as={showConfirmPassword ? EyeIcon : EyeOffIcon} />
+//             </InputSlot>
+//           </Input>
+//           {errors.confirmPassword && (
+//             <FormControlErrorText>
+//               {errors.confirmPassword}
+//             </FormControlErrorText>
+//           )}
+//         </VStack>
+//         <VStack space="xs">
+//           <Checkbox
+//             size="md"
+//             isChecked={formData.termsAccepted}
+//             onChange={() =>
+//               handleChange("termsAccepted", !formData.termsAccepted)
+//             }
+//           >
+//             <CheckboxIndicator>
+//               <CheckboxIcon as={CheckIcon} />
+//             </CheckboxIndicator>
+//             <CheckboxLabel>I agree to terms and conditions</CheckboxLabel>
+//           </Checkbox>
+//           {errors.termsAccepted && (
+//             <FormControlErrorText>{errors.termsAccepted}</FormControlErrorText>
+//           )}
+//         </VStack>
+//       </FormControl>
+
+//       <Button
+//         className="w-full self-center mt-4 bg-[#259e47] rounded-lg"
+//         size="sm"
+//         onPress={handleSubmit}
+//         isDisabled={
+//           !formData.firstName ||
+//           !formData.firstName ||
+//           !formData.firstName ||
+//           !formData.firstName ||
+//           !formData.password ||
+//           !formData.confirmPassword ||
+//           !formData.termsAccepted
+//         }
+//       >
+//         <ButtonText>Sign Up</ButtonText>
+//       </Button>
+
+//       {/* OR Divider */}
+//       <HStack className="flex items-center my-4">
+//         <Divider className="flex-1" />
+//         <Text className="font-semibold mx-2">Or</Text>
+//         <Divider className="flex-1" />
+//       </HStack>
+
+//       <VStack space="sm" className="mt-4">
+//         <Pressable className="p-4 border border-outline-200 rounded-full flex-row items-center justify-center">
+//           <AntDesign name="google" size={24} color="black" />
+//           <Text className="ml-2">Continue with Google</Text>
+//         </Pressable>
+//         <Pressable className="p-4 border border-outline-200 rounded-full flex-row items-center justify-center">
+//           <AntDesign name="apple1" size={24} color="black" />
+//           <Text className="ml-2">Continue with Apple</Text>
+//         </Pressable>
+//       </VStack>
+//     </VStack>
+//   );
+// };
+
+// export default SignInForm;
--- screens/GetStartedScreen.js
+++ screens/GetStartedScreen.js
@@ -1,68 +1,50 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useRef } from "react";
+import { SafeAreaView, ScrollView, View, Text, StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  interpolate,
+  useAnimatedScrollHandler,
+} from "react-native-reanimated";
 import GetStartedTabs from "@/auction-components/GetStartedTabs";
-import { COLORS } from "@/constants/COLORS";
 
 const GetStartedScreen = () => {
   return (
-    <SafeAreaView className="flex-1  h-full w-full ">
-      {/* Full-height container */}
+    <SafeAreaView className="flex-1">
       {/* Top Section */}
-      {/* <View className="h-[30%] flex flex-col  bg-[#259e47] p-8 justify-center items-center">
-        <Text className="text-white text-3xl font-bold">QuickAuct</Text>
-        <Text className="text-white text-lg">Get Started now</Text>
-        <Text className="text-xl text-white text-center px-5">
-          Create an account or login to view auctions
-        </Text>
-      </View> */}
-      <View style={styles.header}>
-        <Text style={styles.title} className="text-white text-3xl font-bold">
-          QuickAuct
-        </Text>
-        <Text style={styles.title} className="text-white text-lg">
-          Get Started now
-        </Text>
-        <Text className="text-xl text-white text-center px-5">
-          Create an account or login to view auctions
-        </Text>
-      </View>
-      <View className="h-[60%] py-5 px-8 justify-center items-center">
-        <GetStartedTabs />
-      </View>
+
+      <ScrollView>
+        <View className="h-[30%] bg-[#259e47] py-4 px-6">
+          <Text
+            style={styles.title}
+            className="text-white text-xl font-semibold "
+          >
+            QuickAuct
+          </Text>
+          <Text
+            style={styles.title}
+            className="text-white text-3xl font-bold mt-4 mb-2"
+          >
+            Get Started now
+          </Text>
+          <Text style={styles.title} className="text-lg text-white">
+            Create an account or login to view auctions
+          </Text>
+        </View>
+
+        {/* forms */}
+        <View className="h-[60%] py-5 px-2 justify-center items-center rounded-t-lg  ">
+          <GetStartedTabs />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
-
 export default GetStartedScreen;
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-  header: {
-    height: "30%",
-    backgroundColor: "#3B82F6", // Equivalent to bg-blue-500
-    justifyContent: "center",
-    alignItems: "center",
-  },
   title: {
-    fontSize: 24,
-    fontWeight: "bold",
+    fontSize: 16,
+    // fontWeight: "bold",
     color: "white",
-  },
-  subtitle: {
-    fontSize: 18,
-    color: "white",
-  },
-  description: {
-    fontSize: 20,
-    color: "#259e47",
-    textAlign: "center",
-    paddingHorizontal: 20,
-  },
-  footer: {
-    height: "60%",
-    paddingVertical: 20,
-    paddingHorizontal: 32,
   },
 });
