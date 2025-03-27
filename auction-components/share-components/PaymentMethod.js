68ce04efaa4c928eabc5ad08b1578bde649ad148
Laouali <laoualibachir2000@gmail.com>
2025-03-27 22:51:05 +0100

    refactor payment method views to a component

---
+++ auction-components/share-components/PaymentMethod.js
@@ -0,0 +1,35 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+const PaymentMethod = ({ icon, label }) => {
+  return (
+    <TouchableOpacity style={styles.paymentMethod}>
+      {icon && <Image source={icon} style={styles.paymentIcon} />}
+      {label && <Text style={styles.paymentLabel}>{label}</Text>}
+    </TouchableOpacity>
+  );
+};
+
+export default PaymentMethod;
+
+const styles = StyleSheet.create({
+  paymentMethod: {
+    flex: 1,
+    height: 60,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+  },
+  paymentIcon: {
+    width: 30,
+    height: 30,
+    marginBottom: 4,
+  },
+  paymentLabel: {
+    fontSize: 12,
+    textAlign: "center",
+  },
+});
